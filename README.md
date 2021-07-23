bea5f290030e413a0720d4390fac50135cda1868
Steffen Froemer <steffen@redhat.com>
2021-07-23 16:29:15 +0200

    updated documentation; added preparation function

--- README.md
+++ README.md
@@ -1,10 +1,69 @@
-# rhvh-custom-iso
-Customize RHVH ISO to include kickstarts for specific environments
 
-Instructions for adding custom kickstart files to the RHV-H ISO,
-including a generic kickstart file showing some options available.
+# Building a Custom Boot ISO for Red Hat Virtualization Hypervisor
+This repo provides a script to automate the customization and build process of a custom RHV-H ISO.
+The work is based on the [Blog Article - Building a Custom Boot ISO for Red Hat Virtualization Hypervisor](https://www.redhat.com/en/blog/building-custom-boot-iso-red-hat-virtualization-hypervisor) by [Marc Skinner](https://www.redhat.com/en/authors/marc-skinner)
 
-Script included to build the new ISO from modified files.
 
-Starting work on code to prepare a BUILD directory from scratch using
-RHV-H ISO downloaded from access.redhat.com.
+# Using the script
+The script has a builtin __usage__ function, which shows the basic usage of that script.
+Note the script should be executed with super-user privileges.
+
+~~~
+$ ./build-iso.sh -h
+This script must be run with super-user privileges.
+Usage: build-iso.sh [-bh] [-p rhv.iso]
+  -b         build RHV-H custom ISO
+  -h         display help
+  -p rhv.iso prepare the environment to customize ISO
+             before building
+~~~
+
+## Prerequisites
+
+### Install all required packages
+Please make sure, you have pre-installed the following packages on your build-system.
+* `syslinux` - Simple kernel loader which boots from a FAT filesystem
+* `isomd5sum` - Utilities for working with md5sum implanted in ISO images
+* `genisoimage` - Creates an image of an ISO9660 file-system
+
+
+### Download the RHV-H image from Red Hat
+The images can be downloaded from [access.redhat.com](https://access.redhat.com/products/red-hat-virtualization#getstarted)
+
+### Setup variables
+* configure the `build-iso.rc` file to setup necessary variables
+
+~~~
+# Path to working directory
+ISO_BASEDIR="/tmp/RHVH-custom"
+# filename of created RHV-H custom image
+ISO_OUT="RHVH-4.4-custom.x86_64.iso"
+~~~
+
+## Start the preparation process
+This process will Copy the content from RHV image into a working directory, which will be used for modifications and build.
+
+~~~
+$ sudo ./build-iso.sh -p ~/Downloads/RHVH-4.4-20201117.0-RHVH-x86_64-dvd1.iso
+~~~
+
+## Perform your customization
+All file in `modification` directory will be included into the image.
+
+To customize the `ks.cfg` it needs to be copied to `modifications`-directory.
+
+~~~
+$ cp ks.cfg /tmp/RHVH-custom/modifications
+~~~
+
+## Run the build process
+To start the build process, execute the following command.
+
+~~~
+# sudo ./build-iso.sh BUILD
+~~~
+
+This will create the ISO in the directory `ISO_BASEDIR` you've configured in `build-iso.rc`
+
+# Disclaimer
+There is no warranty on success by using these scripts. You will use these scripts on your own risk.
--- build-iso.sh
+++ build-iso.sh
@@ -1,28 +1,35 @@
 #!/bin/bash
 #
 # Info: Build RHV-H ISO after modifications are made
-# Author: Jason Woods <jwoods@redhat.com>
+# Author: Jason Woods <jwoods@redhat.com>, Steffen Froemer <steffen@redhat.com>
 # Created: 2018-12-14
+# Updated: 2021-07-23
 # This script has no warranty implied or otherwise.
 #
-# 2019-02-11	jwoods	added function update_efiboot and call to it
-# 2019-02-12	jwoods	added function update_from_mods if ISO_MODDIR exists
-# 2019-02-14	jwoods	changed variables to start with ISO_
-# 2019-02-14	jwoods	added code to read $0.rc file, remove .sh in .rc file name
+# 2021-07-23  sfroemer  added prep- and usage-function, cleanup
+# 2021-07-19  sfroemer  updated iso_name function to properly grab LABEL
+# 2019-02-11  jwoods    added function update_efiboot and call to it
+# 2019-02-12  jwoods    added function update_from_mods if ISO_MODDIR exists
+# 2019-02-14  jwoods    changed variables to start with ISO_
+# 2019-02-14  jwoods    added code to read $0.rc file, remove .sh in .rc file name
 
-# where to find/put stuff
-ISO_BASEDIR="/u/RHV-H_custom"
-ISO_BUILDDIR="BUILD"
-ISO_IN="${ISO_BASEDIR}/RHVH-4.2-20190117.0-RHVH-x86_64-dvd1.iso"
-ISO_OUT="RHVH-4.2-custom.x86_64.iso"
-ISO_MNTDIR="${ISO_BASEDIR}/temp-mnt"
-ISO_MODDIR="${ISO_BASEDIR}/modifications"
+# Path to working directory
+ISO_BASEDIR="/tmp/RHVH-custom"
+# filename of created RHV-H custom image
+ISO_OUT="RHVH-4.4-custom.x86_64.iso"
+
+# Path to the downloaded RHVH image from access.redhat.com (optional)
+# If not configured, the path can be transmitted during preparation process
+ISO_IN=""
 
 # these can be left auto-configured, or changed as desired
-ISO_OUTDIR="${ISO_BASEDIR}/${ISO_BUILDDIR}"
-ISO_OUTPUT="${ISO_BASEDIR}/${ISO_OUT}"
+ISO_SOURCEDIR="${ISO_BASEDIR}/SOURCE"
+ISO_BUILDDIR="${ISO_BASEDIR}/BUILD"
+ISO_MNTDIR="${ISO_BASEDIR}/temp-mnt"
+ISO_MODDIR="${ISO_BASEDIR}/modifications"
+ISO_OUTFILE="${ISO_BASEDIR}/${ISO_OUT}"
 
-# string to use when outputing a note
+# string to use when outputting a note
 ISO_NOTE="#-# "
 
 # if exists and is readable, source .rc file for variables
@@ -31,24 +38,12 @@ ISO_RCFILE="$(echo "$0" | sed 's/\.sh$//;').rc"
 # name of this program
 ISO_PROGNAME="$(basename "${0}")"
 
-case "$(echo "${1}" | sed 's/^-*//;')" in
-prep|PREP|p|P)
-  # default to prep ISO build files
-  ISO_MODE="PREP"
-  ;;
-build|BUILD|b|B|*)
-  # default to build ISO
-  ISO_MODE="BUILD"
-  ;;
-esac
+
 
 function iso_name () {
   # this needs work
-  grep "LABEL=" BUILD/isolinux/isolinux.cfg | head -n1 | \
-    sed 's/:/#/;' | \
-    cut -f2 -d# | \
-    cut -f1 -d' ' | \
-    sed 's/LABEL=//;s/\\x20/ /;'
+  grep "LABEL=" "${ISO_BUILDDIR}/isolinux/isolinux.cfg" | head -n1 | \
+    sed -e 's/^.*:LABEL=\(.*\):.*$/\1/;s/\\x20/ /'
 }
 
 function update_from_mods () {
@@ -57,15 +52,15 @@ function update_from_mods () {
     echo "  ERROR: unable to change to '${ISO_MODDIR}'"
     return
   }
-  find . -type f -exec cp -v "{}" "${ISO_OUTDIR}/{}" \;
+  find . -type f -exec cp -v "{}" "${ISO_BUILDDIR}/{}" \;
   popd >/dev/null
 }
 
 function update_efiboot () {
   echo "${ISO_NOTE}Updating EFIBOOT image ..."
-  [ ! -d "${ISO_MNTDIR}" ] && mkdir "${ISO_MNTDIR}"
-  mount "${ISO_OUTDIR}/images/efiboot.img" "${ISO_MNTDIR}" && {
-   cp "${ISO_OUTDIR}/EFI/BOOT/grub.cfg" "${ISO_MNTDIR}/EFI/BOOT/grub.cfg"
+  [ ! -d "${ISO_MNTDIR}" ] && mkdir -p "${ISO_MNTDIR}"
+  mount "${ISO_BUILDDIR}/images/efiboot.img" "${ISO_MNTDIR}" && {
+   cp "${ISO_BUILDDIR}/EFI/BOOT/grub.cfg" "${ISO_MNTDIR}/EFI/BOOT/grub.cfg"
   } || {
     echo "  ERROR: unable to mount efiboot.img"
   }
@@ -84,19 +79,19 @@ function iso_build () {
   echo "${ISO_NOTE}Generating ISO image ..."
   genisoimage \
     -follow-links \
-    -o "${ISO_OUTPUT}" -joliet-long -b "isolinux/isolinux.bin" \
+    -o "${ISO_OUTFILE}" -joliet-long -b "isolinux/isolinux.bin" \
     -c "isolinux/boot.cat" -no-emul-boot -boot-load-size 4 \
     -boot-info-table -eltorito-alt-boot \
     -e "images/efiboot.img" -no-emul-boot -R -J -v -T \
     -input-charset utf-8 \
     -V "${ISO_NAME}" -A "${ISO_NAME}" \
-    "${ISO_OUTDIR}"
+    "${ISO_BUILDDIR}"
   if [ $? = 0 ] ; then
     echo "${ISO_NOTE}Making ISO UEFI bootable ..."
-    isohybrid -uefi "${ISO_OUTPUT}"
+    isohybrid -uefi "${ISO_OUTFILE}"
     echo "  EXIT: $?"
     echo "${ISO_NOTE}Adding MD5 to ISO ..."
-    implantisomd5 "${ISO_OUTPUT}"
+    implantisomd5 "${ISO_OUTFILE}"
     echo "  EXIT: $?"
   else
     echo
@@ -107,8 +102,33 @@ function iso_build () {
 
 function iso_prep () {
   # prep files for building ISO
-# TMPTMP
-echo "PREPPING!"
+  echo "${ISO_NOTE}Setup prerequisites to customize ISO"
+  echo "${ISO_NOTE}Mounting and copying content from  RHV-H ISO to source directory"
+  [ ! -d "${ISO_MNTDIR}" ] && mkdir -p ${ISO_MNTDIR}
+  [ ! -d "${ISO_BUILDDIR}" ] && mkdir -p ${ISO_BUILDDIR}
+  [ ! -d "${ISO_MODDIR}" ] && mkdir -p ${ISO_MODDIR}
+  [ -z "${ISO_IN}" ] && {
+    echo "  ERROR: Please provide path to RHV-H image."
+    exit 1
+  }
+  mount ${ISO_IN} ${ISO_MNTDIR} >/dev/null 2>&1 && {
+    cp -avr ${ISO_MNTDIR}/* ${ISO_BUILDDIR}/
+  } || {
+    echo "  ERROR: unable to mount ${ISO_IN}"
+    exit 1
+  }
+  umount "${ISO_MNTDIR}" && echo "  SUCCESS"
+}
+
+function usage () {
+  # print small usage documentation
+  echo "This script must be run with super-user privileges." 
+  echo "Usage: ${ISO_PROGNAME} [-bh] [-p rhv.iso]" 
+  echo "  -b         build RHV-H custom ISO"
+  echo "  -h         display help"
+  echo "  -p rhv.iso prepare the environment to customize ISO"
+  echo "             before building"
+  exit 1
 }
 
 function main () {
@@ -117,16 +137,22 @@ function main () {
     echo "${ISO_NOTE}Used ISO_RCFILE='${ISO_RCFILE}'"
     source "${ISO_RCFILE}"
   fi
-  # run according to BUILD or PREP mode
-  case "${ISO_MODE}" in
-  PREP)
+
+  case "$(echo "${1}" | sed 's/^-*//;')" in
+  prep|PREP|p|P)
+    # default to prep ISO build files
+    [[ -n "${2}" ]] && ISO_IN="$(dirname $2)/$(basename $2)"
     iso_prep
-  ;;
-  BUILD|*)
+    ;;
+  build|BUILD|b|B)
+    # default to build ISO
     iso_build
-  ;;
+    ;;
+  h|help|*)
+    # call help function
+    usage
+    ;;
   esac
 }
 
-main 2>&1 | tee build.out
-
+main $@ 2>&1 | tee build.out
